4c28715759ad0a613684eeae68b5f1602a91604c
Eduardo André <eduardoandre418@gmail.com>
2023-04-01 00:49:03 -0300

    vou salvar o javascripy

--- scripy.js
+++ scripy.js
@@ -1,7 +1,36 @@
-function select(clicado){
-    const anterior = document.querySelector("selecionado");
-    if (anterior != null) {
-        anterior.classList.toggle("selecionado");
+
+function selectprato(clicado){
+    const anterior = document.querySelector(".selecionado");
+    const pratoprincipal = document.querySelector(".pratoprincipal")
+    if (anterior != null && pratoprincipal == null){
+        anterior.classList.remove("selecionado");
+        pratoprincipal.classList.add("pratoprincipal");
+        alert("prato");
     }
     clicado.classList.add("selecionado");
+    pratoprincipal.classList.remove("pratoprincipal");
+}
+function selectbebida(clicado){
+    const anterior = document.querySelector(".selecionado");
+    const bebida = document.querySelector(".bebida .itens");
+    if (bebida != null ){
+        anterior.classList.remove("selecionado");
+        bebida.classList.remove("bebida");
+        alert("bebida")
     }
+    clicado.classList.add("selecionado bebida");
+    bebida.classList.remove("bebida");    
+}
+
+function selectsobremesa(clicado){
+    const anterior2 = document.querySelector(".selecionado");
+    const sobremesa = document.querySelector(".sobremesa");
+    if (anterior2 != null && sobremesa != null) {
+        anterior2.classList.remove("selecionado");
+        sobremesa.classList.remove("sobremesa");
+    }
+    clicado.classList.add("selecionado");
+    sobremesa.classList.remove("sobremesa");    
+}
+
+
