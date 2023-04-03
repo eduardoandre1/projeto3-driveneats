dd64275e461a64c26bc12a3216237b50608564ed
Eduardo André <eduardoandre418@gmail.com>
2023-04-03 13:10:27 -0300

    mensagem de whatsaap personalizada

--- scripy.js
+++ scripy.js
@@ -1,11 +1,17 @@
 function selectprato(clicado){
     const anterior = document.querySelector(".pratoprincipal")
+    const nome = document.querySelector(".pratoprincipal h2")
     if(anterior != null ){
         anterior.classList.remove("selecionado")
         anterior.classList.remove("pratoprincipal")
+
     }
+    /*const nome = document.querySelector(".pratoprincipal h2")
+    nome.innerHTML("esta funcionando")*/
     clicado.classList.add("selecionado")
     clicado.classList.add("pratoprincipal")
+    alert(nome.innerHTML)
+
 }
 function selectbebida(clicado){
     const anterior = document.querySelector(".bebida")
@@ -37,4 +43,11 @@ function finalizar_pedido(){
         document.getElementById("botão").disabled = false;
     }
 }
-
+function enviarMensagem() {
+    const numero = "5521970124125";
+    const mensagem = "Olá, gostaria de fazer o pedido ";
+    const pratonome = document.querySelector(".pratoprincipal h2").innerHTML
+    const bebidanome = document.querySelector(".bebida h2").innerHTML
+    const sobremesanome = document.querySelector(".sobremesa h2").innerHTML
+    window.open("https://web.whatsapp.com/send?phone=" + numero + "&text=" + mensagem + pratonome + ", "+bebidanome +", "+ sobremesanome);
+}
